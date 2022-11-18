c4b94cbb956530e2cb95f0c82b4cb81a0e004c8c
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2022-11-18 15:21:52 -0300

    :sparkles: feat: Adicionando hash nas senhas

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -3,6 +3,7 @@ import connectionSource from "../database";
 import { Account } from "../entities/Account";
 import { User } from "../entities/User";
 import { IUserService, IUserRequest } from "../protocols";
+import bcrypt from "bcrypt";
 
 export default class UserService implements IUserService {
   repositoryAccount: Repository<Account>
@@ -15,10 +16,11 @@ export default class UserService implements IUserService {
   async create(user: IUserRequest): Promise<User> {
     const account = this.repositoryAccount.create({ balance: 100.00 });
     await this.repositoryAccount.save(account);
+    const hashedPassword = bcrypt.hashSync(user.password, 10);
 
     const newUser = this.repositoryUser.create({
       username: user.username,
-      password: user.password,
+      password: hashedPassword,
       accountId: account.id
     });
     await this.repositoryUser.save(newUser);
