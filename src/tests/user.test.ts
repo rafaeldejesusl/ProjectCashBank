6bd82529ea590fc27c828c79ec1f468455723b5d
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2022-12-19 16:13:20 -0300

    :test_tube: test: Adicionando teste de quando a senha não tiver letras maiúsculas

--- src/tests/user.test.ts
+++ src/tests/user.test.ts
@@ -76,16 +76,6 @@ describe('Model User', () => {
     expect(response.status).to.be.equal(400);
     expect(response.body.message).to.be.equal('Invalid username');
   });
-});
-
-describe('Model User', () => {
-  before(() => {
-    sinon.stub(repositoryUser, 'find').resolves([]);
-  });
-
-  after(() => {
-    (repositoryUser.find as sinon.SinonStub).restore();
-  });
 
   it('metodo post /user quando a senha for muito curta', async () => {
     const response = await chai.request(app).post('/user')
@@ -97,6 +87,13 @@ describe('Model User', () => {
   it('metodo post /user quando a senha não tiver números', async () => {
     const response = await chai.request(app).post('/user')
       .send({ username: userMock.username, password: 'Batatinha' });
+    expect(response.status).to.be.equal(400);
+    expect(response.body.message).to.be.equal('Invalid password');
+  });
+
+  it('metodo post /user quando a senha não tiver letras maiúsculas', async () => {
+    const response = await chai.request(app).post('/user')
+      .send({ username: userMock.username, password: 'batatinha123' });
     expect(response.status).to.be.equal(400);
     expect(response.body.message).to.be.equal('Invalid password');
   });
